3755e039b2eb21d1c7a306ffc3cf04dea234f4c3
Nikita Platonov <fifaakkaynt92@gmail.com>
2023-03-29 18:12:52 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,23 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class Main {
     public static List<Person> generateClients(ArrayList<Person> clients) {
+        System.out.println("Список клиентов");
+        System.out.println();
         for (int i = 0; i < clients.size(); i++) {
-            System.out.println((i + 1) + ". " + clients.get(i));
+            System.out.println(clients.get(i).getName());
         }
         return clients;
     }
-    public static void main (String[]args){
+
+    public static void main(String[] args) {
 
         ArrayList<Person> queueClients = new ArrayList<>();
         Person Andrey = new Person("Andrey", "Andreev", 5);
         Person Nikita = new Person("Nikita", "Platonov", 2);
-        Person Vika = new Person("Vika","Vikovna", 3);
-        Person Denis = new Person("Denis","Denisovich", 4);
-        Person Jastin = new Person("Jastin","Biber", 1);
+        Person Vika = new Person("Vika", "Vikovna", 3);
+        Person Denis = new Person("Denis", "Denisovich", 4);
+        Person Jastin = new Person("Jastin", "Biber", 1);
 
         queueClients.add(Andrey);
         queueClients.add(Vika);
@@ -27,6 +27,27 @@ public class Main {
 
         generateClients(queueClients);
 
-        Queue<Person> queueForRides = new ArrayDeque<>(5);
+        ArrayDeque<Person> queueForRides = new ArrayDeque<>();
+
+        for (Person client : queueClients) {
+            queueForRides.offer(client);
+        }
+        while (true) {
+            if (!queueForRides.isEmpty()) {
+                Person client = queueForRides.pollFirst();
+                if (client.countTicket()) {
+                    System.out.println();
+                    System.out.println(client.getName() + " прокатился на аттракционе");
+                    System.out.println();
+                    client.usingTheTicket();
+                    queueForRides.offerLast(client);
+                }
+            }
+
+
+
+        }
     }
+
+
 }
--- src/Person.java
+++ src/Person.java
@@ -1,11 +1,23 @@
 public class Person {
-    String name;
-    String surname;
-    int countTickets;
+    private String name;
+    private String surname;
+    private int countTickets;
 
     public Person(String name, String surname, int countTickets) {
         this.name = name;
         this.surname = surname;
         this.countTickets = countTickets;
+    }
+    String getName () {
+        return name + " " + surname;
+    }
+    boolean countTicket () {
+        if (countTickets > 0) {
+            return true;
+        }
+        return false;
+    }
+    void usingTheTicket () {
+        countTickets = countTickets - 1;
     }
 }
